b45e7fd4835c2600d3fbfbaef1fb36f4eadf58a2
NHBKhang <nhbkhang12@gmail.com>
2024-04-09 23:58:27 +0700

    Update Return

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # My CV Template
+
+A simple static website contains my CV template. It can be download and use for job application.
+
+ - Languages: Html, Css, Javascript
--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 function exportToPDF() {
-    var table = document.querySelector('#cv');
-    html2pdf(table, {
-        margin: 0,
-        filename: 'NguyenHaBaoKhang_CV',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-    }).then(function (canvas) {
-        // Use jsPDF to save the canvas as a PDF
-        var pdf = new jsPDF();
-        pdf.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, 210, 297);
-        pdf.save('NguyenHaBaoKhang_CV.pdf');
-    });
+    if (confirm("Bạn có muốn in CV không?")){
+        var table = document.querySelector('#cv');
+        html2pdf(table, {
+            margin: -2,
+            filename: 'NguyenHaBaoKhang_CV',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        }).then(function (canvas) {
+            // Use jsPDF to save the canvas as a PDF
+            var pdf = new jsPDF();
+            pdf.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, 210, 297);
+            pdf.save('NguyenHaBaoKhang_CV.pdf');
+        });
+    }
 }
 
-function changeColor(className) {
-    var container = document.getElementsByTagName("body")[0];
-    container.classList.remove('red-bg', 'blue-bg', 'green-bg');
-    container.classList.add(className);
+window.onscroll = function () {
+    let r = document.getElementById("return")
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        r.style.display = "block";
+    } else {
+        r.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
