afbe5c810edc5ea80b03a9977e8342354ef9269a
NHBKhang <nhbkhang12@gmail.com>
2024-04-09 23:23:25 +0700

    Update Header and Footer

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ function exportToPDF() {
     html2pdf(table, {
         margin: 0,
         filename: 'NguyenHaBaoKhang_CV',
-        image: {type: 'jpeg', quality: 0.98},
-        html2canvas: {scale: 2},
-        jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait'}
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     }).then(function (canvas) {
         // Use jsPDF to save the canvas as a PDF
         var pdf = new jsPDF();
         pdf.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, 210, 297);
         pdf.save('NguyenHaBaoKhang_CV.pdf');
     });
+}
+
+function changeColor(className) {
+    var container = document.getElementsByTagName("body")[0];
+    container.classList.remove('red-bg', 'blue-bg', 'green-bg');
+    container.classList.add(className);
 }
